242e904f563ddebddf1ee8f639abb5bc84679539
Aaron Cuddeback <midlifecoder@Aarons-MacBook-Pro.local>
2020-02-18 12:18:36 -0800

    change portfolio imgs, delete old imgs

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -18,125 +18,141 @@ export default props => {
         <Row>
           <Col xs="12" sm="5">
             <h1>Artistic Ink</h1>
+            {
+              <img
+                src={require('../images/artisticbanner.jpg')}
+                width={300}
+                height={100}
+                mode="fit"
+              />
+            }
             <p>
+              <br />
               Artistic Ink is a permanent cosmetics company which provides all
               types of permanent cosmetic services.
               <br />
+              <br />
               <a href="https://pmu-artisticink.com/" target="_blank">
-                View Site
+                <center>View Site</center>
               </a>
             </p>
-            {
-              <img
-                src={require('../images/pmu-artisticink.jpg')}
-                width={300}
-                height={900}
-                mode="fit"
-              />
-            }
           </Col>
           <Col xs="12" sm="5">
             <h1>Rear Light Bar</h1>
-            <p>
-              Rear Light Bar is an offroad lightbar company which distributes
-              custom lighting for your 4x4 vehicle.
-              <br />
-              <a href="https://rlbstore.com/" target="_blank">
-                View Site
-              </a>
-            </p>
             {
               <img
-                src={require('../images/rlbstore-min.jpg')}
+                src={require('../images/rlbbanner.jpg')}
                 width={300}
-                height={900}
+                height={100}
                 mode="fit"
               />
             }
+            <p>
+              <br />
+              Rear Light Bar is an offroad lightbar company which distributes
+              custom lighting for your 4x4 vehicle.
+              <br />
+              <br />
+              <a href="https://rlbstore.com/" target="_blank">
+                <center>View Site</center>
+              </a>
+            </p>
+            
           </Col>
         </Row>
         <Row>
           <Col xs="12" sm="5">
             <br />
             <h1>CannaCura</h1>
-            <p>
-              An Ecommerce website dedicated to CBD products, it is a custom
-              Wordpress design.
-              <br />
-              <a href="https://cannacura.net/" target="_blank">
-                View Site
-              </a>
-            </p>
             {
               <img
-                src={require('../images/CannaCura.jpg')}
+                src={require('../images/cannacurabanner.jpg')}
                 width={300}
-                height={900}
+                height={100}
                 mode="fit"
               />
             }
+            <p>
+            <br />
+              An Ecommerce website dedicated to CBD products, it is a custom
+              Wordpress design.
+              <br />
+              <br />
+              <a href="https://cannacura.net/" target="_blank">
+                <center>View Site</center>
+              </a>
+            </p>
           </Col>
           <Col xs="12" sm="5">
             <br />
             <h1>Flavolution</h1>
-            <p>
-              A wonderful health company who sells healthy spices for the
-              fitness community.
-              <br />
-              <a href="https://theflavolution.com/" target="_blank">
-                View Site
-              </a>
-            </p>
             {
               <img
-                src={require('../images/Flavolution.gif')}
+                src={require('../images/flavolutionbanner.jpg')}
                 width={300}
-                height={900}
+                height={100}
                 mode="fit"
               />
             }
+            <p>
+              <br />
+              A wonderful health company who sells healthy spices for the
+              fitness community.
+              <br />
+              <br />
+              <a href="https://theflavolution.com/" target="_blank">
+                <center>View Site</center>
+              </a>
+            </p>
+            
           </Col>
         </Row>
         <Row>
           <Col xs="12" sm="5">
             <br />
             <h1>Hangtown Range</h1>
-            <p>
-              An website designed for a Shooting and Archery Range.
-              <br />
-              <a href="https://hangtownrange.com/" target="_blank">
-                View Site
-              </a>
-            </p>
-
             {
               <img
-                src={require('../images/hangtownRange.jpg')}
+                src={require('../images/hangtownbanner.jpg')}
                 width={300}
-                height={1000}
+                height={100}
                 mode="fit"
               />
             }
+            <p>
+              <br />
+              An website designed for a Shooting and Archery Range.
+              <br />
+              <br />
+              <a href="https://hangtownrange.com/" target="_blank">
+                <center>View Site</center>
+              </a>
+            </p>
+
+           
           </Col>
           <Col xs="12" sm="5">
             <br />
             <h1>Norcal Drone Solutions</h1>
-            <p>
-              A website built and designed for a Northern California Drone
-              company.
-              <br />
-              <a href="https://hirenorcaldrones.com/" target="_blank">
-                View Site
-              </a>
-            </p>
             {
               <img
-                src={require('../images/hirenorcaldrones.gif')}
+                src={require('../images/dronebanner.jpg')}
                 width={300}
-                height={900}
+                height={100}
                 mode="fit"
               />
             }
+            <p>
+              <br />
+              A website built and designed for a Northern California Drone
+              company.
+              <br />
+              <br />
+              <a href="https://hirenorcaldrones.com/" target="_blank">
+                <center>View Site</center>
+              </a>
+            </p>
+           
           </Col>
         </Row>
       </Container>
